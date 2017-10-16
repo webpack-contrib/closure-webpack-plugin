c808d72023117a88b0a4e0c74168cfcc56953dca
Chad Killingsworth <chadkillingsworth@gmail.com>
2017-10-15 22:26:24 -0500

    Avoid using Object.values - it's not supported on Node 6.

--- src/amd-define-dependency-template.js
+++ src/amd-define-dependency-template.js
@@ -3,15 +3,17 @@ const AMDDefineDependency = require('webpack/lib/dependencies/AMDDefineDependenc
 class AMDDefineDependencyTemplate extends AMDDefineDependency.Template {
   get definitions() { // eslint-disable-line  class-methods-use-this
     const defs = super.definitions;
-    Object.values(defs).forEach((value) => {
-      const valueEntries = value;
-      value.forEach((line, index) => {
-        if (!/^var/.test(line)) {
-          return;
-        }
-        valueEntries[index] = line.replace(/var __WEBPACK_AMD/g, '/** @suppress {duplicate} */$&');
-      });
-    });
+    for (let value in defs) { // eslint-disable-line prefer-const
+      if (Object.prototype.hasOwnProperty.call(defs, value)) {
+        const valueEntries = defs[value];
+        defs[value].forEach((line, index) => {
+          if (!/^var/.test(line)) {
+            return;
+          }
+          valueEntries[index] = line.replace(/var __WEBPACK_AMD/g, '/** @suppress {duplicate} */$&');
+        });
+      }
+    }
     return defs;
   }
 }

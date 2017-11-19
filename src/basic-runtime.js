8eca44edc4d4199c8fd67d6b1a6f486ec3379408
Chad Killingsworth <chadkillingsworth@gmail.com>
2017-11-19 17:26:36 -0600

    fix: better logic around the mode option
    Also add a very basic runtime when no late loaded modules are present.

---
+++ src/basic-runtime.js
@@ -0,0 +1,10 @@
+/* eslint-env browser */
+/* eslint no-underscore-dangle: "off", camelcase: "off", no-var: "off" */
+// webpackBootstrap
+var __webpack_require__;
+if (typeof __webpack_require__ === 'undefined') {
+  __webpack_require__ = function (m) {}; // eslint-disable-line func-names, no-unused-vars
+}
+
+/** @define {string} public path */
+__webpack_require__.p = '';

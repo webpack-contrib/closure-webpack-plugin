4392f765536c603da66d0e56cc6a2589bf7f11f2
Chad Killingsworth <chadkillingsworth@gmail.com>
2018-10-07 07:15:43 -0500

    fix: correct asset naming when using a custom naming template

--- src/index.js
+++ src/index.js
@@ -274,10 +274,7 @@ class ClosureCompilerPlugin {
               } else {
                 assetName = outputFile.path.replace(/^\.\//, '');
                 if (!/\.js$/.test(chunk.files[0])) {
-                  assetName = outputFile.path.substr(
-                    0,
-                    outputFile.path.length - 3
-                  );
+                  assetName = assetName.substr(0, assetName.length - 3);
                 }
               }
               const sourceMap = JSON.parse(outputFile.source_map);

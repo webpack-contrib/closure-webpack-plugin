3c0aa58aab824322de479f982a8a78d20ff0f981
Chad Killingsworth <chadkillingsworth@gmail.com>
2018-11-30 07:35:16 -0600

    fix: minor fix for source paths

--- src/safe-path.js
+++ src/safe-path.js
@@ -2,7 +2,7 @@
  * @fileoverview
  * Given a path, replace all characters not recognized by closure-compiler with a '$'
  */
-const UNSAFE_PATH_CHARS = /[^-a-z0-9_$/\\.:]+/gi;
+const UNSAFE_PATH_CHARS = /[^-a-z0-9_$@/\\.:]+/gi;
 module.exports = function toSafePath(originalPath) {
   return originalPath.replace(UNSAFE_PATH_CHARS, '$');
 };

ea011d1c940982380e7d01bb41482c5c8c5150f2
meng_z <meng_z@worksap.co.jp>
2018-11-30 14:44:51 +0800

    fix: fix a js error

--- src/index.js
+++ src/index.js
@@ -438,7 +438,7 @@ Use the CommonsChunkPlugin to ensure a module exists in only one bundle.`,
         });
 
         if (
-          this.options.entryChunks.indexOf(chunk.id) < 0 &&
+          this.options.entryChunks.indexOf(chunk.name) < 0 &&
           parentChunk !== null
         ) {
           return `${defParts[0]}:webpackJsonp([${

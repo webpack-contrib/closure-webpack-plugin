df1861b89c11fb8329df1466691b9205e4e7b986
Chad Killingsworth <chadkillingsworth@gmail.com>
2019-12-21 16:01:52 -0600

    fix: ensure that the goog.global property references window

--- src/dependencies/goog-base-global.js
+++ src/dependencies/goog-base-global.js
@@ -4,12 +4,24 @@ class GoogBaseGlobalDependency extends Dependency {}
 
 class GoogBaseGlobalDependencyTemplate {
   apply(dep, source) {
+    const sourceContent = source.source();
     const content = `goog.ENABLE_DEBUG_LOADER = false;
-goog.global = window;
-window.goog = goog;
 module.exports = goog;`;
-    source.insert(source.source().length, content);
-    source.insert(0, 'this.CLOSURE_NO_DEPS = true;\n');
+    source.insert(sourceContent.length, content);
+
+    const globalDefIndex = sourceContent.search(/\n\s*goog\.global\s*=\s*/);
+    let statementEndIndex = -1;
+    if (globalDefIndex >= 0) {
+      statementEndIndex = sourceContent.indexOf(';', globalDefIndex);
+    }
+    if (statementEndIndex) {
+      source.insert(
+        statementEndIndex + 1,
+        'goog.global = window; goog.global.CLOSURE_NO_DEPS = true;'
+      );
+    } else {
+      source.insert(0, 'this.CLOSURE_NO_DEPS = true;\n');
+    }
   }
 }
 

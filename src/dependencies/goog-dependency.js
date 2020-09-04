30df3051befaba33cb26121bcb7508e90383805a
Kevin Schmidt <kschmidt@caci.com>
2020-09-04 09:59:38 -0600

    fix: include goog.requireType in build dependencies
    Fixes #140

--- src/dependencies/goog-dependency.js
+++ src/dependencies/goog-dependency.js
@@ -1,10 +1,11 @@
 const ModuleDependency = require('webpack/lib/dependencies/ModuleDependency');
 
 class GoogDependency extends ModuleDependency {
-  constructor(request, insertPosition, isBase = false) {
+  constructor(request, insertPosition, isBase = false, isRequireType = false) {
     super(request);
     this.insertPosition = insertPosition;
     this.isBase = isBase;
+    this.isRequireType = isRequireType;
   }
 
   get type() {
@@ -14,12 +15,18 @@ class GoogDependency extends ModuleDependency {
   updateHash(hash) {
     hash.update(this.insertPosition + '');
     hash.update(this.isBase + '');
+    hash.update(this.isRequireType + '');
   }
 }
 
 class GoogDependencyTemplate {
   apply(dep, source) {
     if (dep.insertPosition === null) {
+      return;
+    }
+
+    // goog.requireType is an implicit dependency and shouldn't be loaded
+    if (dep.isRequireType) {
       return;
     }
 

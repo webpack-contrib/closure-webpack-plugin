8a62233effb683922ced1e7322bc75b5131efbf4
Hitko Development <development@hitko.si>
2021-08-03 14:36:53 +0200

    fix: compile on windows when path contains |
    Fixes error with paths like `webpack://javascript/esm|C:/Development/...`.

--- src/safe-path.js
+++ src/safe-path.js
@@ -3,5 +3,5 @@
  * Given a path, replace all characters not recognized by closure-compiler with a '$'
  */
 module.exports = function toSafePath(originalPath) {
-  return originalPath.replace(/[:,?]+/g, '$');
+  return originalPath.replace(/[:,?|]+/g, '$');
 };
